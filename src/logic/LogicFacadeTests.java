1289ea61aedb16b7b0bea6308e06c1ef1a4a5c8a
David Zwerdling <zwerdlds@gmail.com>
2022-06-30 10:09:15 -0700

    Renamed ActionFacade to LogicFacade

--- src/logic/LogicFacadeTests.java
+++ src/logic/LogicFacadeTests.java
@@ -3,9 +3,11 @@ package logic;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-public class ActionFacadeTests {
+public class LogicFacadeTests {
     @Test
-    void testSetup() {
+    void testBasic() {
+        var f = new LogicFacade();
+
         assertTrue(true);
     }
 }
