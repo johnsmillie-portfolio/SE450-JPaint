65d3f502c2569b509b1d5f4ab0ae674f8039a65b
David Zwerdling <zwerdlds@gmail.com>
2022-06-30 09:11:38 -0700

    added paint() method to PaintCanvas class per Professor Sharpe's instructions

--- src/view/gui/PaintCanvas.java
+++ src/view/gui/PaintCanvas.java
@@ -8,6 +8,31 @@ import java.awt.*;
 public class PaintCanvas extends PaintCanvasBase {
 
     public Graphics2D getGraphics2D() {
-        return (Graphics2D)getGraphics();
+        return (Graphics2D) getGraphics();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Graphics2D graphics2d = (Graphics2D) g;
+
+        // Draw all shapes here
+
+        // For example purposes only; remove all lines below from your final project.
+        graphics2d.setColor(Color.GREEN);
+        graphics2d.fillRect(12, 13, 200, 400);
+
+        // Outlined rectangle
+        graphics2d.setStroke(new BasicStroke(5));
+        graphics2d.setColor(Color.BLUE);
+        graphics2d.drawRect(12, 13, 200, 400);
+
+        // Selected Shape
+        Stroke stroke = new BasicStroke(3,
+                BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_BEVEL, 1,
+                new float[] { 9 }, 0);
+        graphics2d.setStroke(stroke);
+        graphics2d.setColor(Color.BLACK);
+        graphics2d.drawRect(7, 8, 210, 410);
     }
 }
