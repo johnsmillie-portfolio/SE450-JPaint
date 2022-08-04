cd93f361e99a2e0f62dacfb42a7f7c622f6b0afe
jsmillie23 <76675746+jsmillie23@users.noreply.github.com>
2022-08-04 09:11:11 -0500

    cloneShape functionality added to PaintShape interface

--- src/view/interfaces/IPaintShape.java
+++ src/view/interfaces/IPaintShape.java
@@ -3,7 +3,9 @@ package view.interfaces;
 import java.awt.Point;
 
 public interface IPaintShape {
+    
     public void paint(PaintCanvasBase c);
     public boolean collides(Point from, Point to);
     public void move(int x, int y);
+    IPaintShape cloneShape();
 }
